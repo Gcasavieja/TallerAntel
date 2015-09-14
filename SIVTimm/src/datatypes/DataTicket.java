1165a0104e154f0ce8d8d33c1a4f2e3dd156ff9e
d450455 <d450455@X1507275.net.in.iantel.com.uy>
2015-09-14 15:31:34 -0300

    attr fecha

--- SIVTimm/src/datatypes/DataTicket.java
+++ SIVTimm/src/datatypes/DataTicket.java
@@ -1,11 +1,22 @@
 package datatypes;
 
+import java.util.Date;
+
 public class DataTicket {
 	private String monto;
 	private String matricula;
+	private Date fechaVenta;
 
 	public String getMonto() {
 		return monto;
+	}
+
+	public Date getFechaVenta() {
+		return fechaVenta;
+	}
+
+	public void setFechaVenta(Date fechaVenta) {
+		this.fechaVenta = fechaVenta;
 	}
 
 	public void setMonto(String monto) {
