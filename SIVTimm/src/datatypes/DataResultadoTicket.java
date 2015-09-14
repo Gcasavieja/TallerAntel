546a375691acf05de74c2238c3e8a7ead2403551
d450455 <d450455@X1507275.net.in.iantel.com.uy>
2015-09-14 15:36:17 -0300

    esto es una prueba

--- SIVTimm/src/datatypes/DataResultadoTicket.java
+++ SIVTimm/src/datatypes/DataResultadoTicket.java
@@ -1,5 +1,14 @@
 package datatypes;
 
 public class DataResultadoTicket {
+	private String prueba;
 
+	public String getPrueba() {
+		return prueba;
+	}
+
+	public void setPrueba(String prueba) {
+		this.prueba = prueba;
+	}
+	
 }
