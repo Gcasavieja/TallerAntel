29d485e18d6c3948a206705b9a7779d13c3a711e
d450455 <d450455@X1507275.net.in.iantel.com.uy>
2015-09-14 15:23:06 -0300

    prueba

--- SIVTimm/src/datatypes/DataTicket.java
+++ SIVTimm/src/datatypes/DataTicket.java
@@ -1,5 +1,14 @@
 package datatypes;
 
 public class DataTicket {
+	private String monto;
 
+	public String getMonto() {
+		return monto;
+	}
+
+	public void setMonto(String monto) {
+		this.monto = monto;
+	}
+	
 }
